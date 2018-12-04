0876008b2af919b555c8dd3eef16c4eddf6c3f47
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-04 22:09:42 +0000

    Adding GET route to show the form and creating random alphanumeric generator.

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 let urlDatabase = {
@@ -21,9 +23,18 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -35,3 +46,11 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+function generateRandomString() {
+  let alphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+  let string = '';
+  for (let x = 0; x < 6; x++) {
+    string += alphaNumeric[Math.floor(Math.random() * alphaNumeric.length)];
+  };
+  return string;
+}
