283198a9591b4ffa76b95261561b64e14a5378ed
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-05 21:29:07 +0000

    To implementing login and logout functionality.

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 let urlDatabase = {
@@ -30,17 +32,38 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie('username', username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase};
+  let templateVars = {
+    'urls': urlDatabase,
+    'username': req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    'username': req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  let templateVars = {
+    'shortURL': req.params.id,
+    'longURL': urlDatabase[req.params.id],
+    'username': req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -48,12 +71,17 @@ app.post("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
   let longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
+  console.log(req.cookie);
+// res.cookie('username', req.cookies['username']);
   res.redirect("/urls");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
-  let templateVars = {urls: urlDatabase};
+  let templateVars = {
+    'urls': urlDatabase,
+    'username': req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
